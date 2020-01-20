112be5646b806177b959084b6e06ed5aea65e177
Marco Winzker <57663666+Marco-Winzker@users.noreply.github.com>
2020-01-20 10:50:18 +0100

    Add files via upload

--- Verification/sharp_generate_testbench_images.m
+++ Verification/sharp_generate_testbench_images.m
@@ -9,6 +9,7 @@ fprintf('Edge enhancement with vertical and horizontal FIR-filter\n')
 
 pkg load image;
 img_in = imread("Lindau_Harbour_720p.jpg"); % change name for your test image
+% 1280 * 720 pixel (720p) recommended for use with remote lab
 
 f_hor = [1, 0, -9, 48, -9, 0, 1]/32;
 f_ver = f_hor'; % transpose filter matrix
