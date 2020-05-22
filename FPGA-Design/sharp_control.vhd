354583bc3a026de0d83da63f97dd79ccd99581c3
Marco Winzker <57663666+Marco-Winzker@users.noreply.github.com>
2020-05-22 18:29:35 +0200

    Add files via upload
    22.05.2020 - Marco Winzker - assignment of input signals now inside process to avoid simulation problem, minimum delay is 2

--- FPGA-Design/sharp_control.vhd
+++ FPGA-Design/sharp_control.vhd
@@ -2,6 +2,8 @@
 --
 -- FPGA Vision Remote Lab http://h-brs.de/fpga-vision-lab
 -- (c) Marco Winzker, Hochschule Bonn-Rhein-Sieg, 10.01.2020
+--
+-- 22.05.2020 - Marco Winzker - assignment of input signals now inside process to avoid simulation problem, minimum delay is 2
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -22,32 +24,34 @@ end sharp_control;
 architecture behave of sharp_control is
 
 
-  type delay_array is array (0 to delay-1) of std_logic;
+  type delay_array is array (1 to delay) of std_logic;
   signal vs_delay : delay_array;
   signal hs_delay : delay_array;
   signal de_delay : delay_array;
 
 begin
 
-process
-begin
-  wait until rising_edge(clk);
+  process
+  begin
+    wait until rising_edge(clk);
 
-  vs_delay(0) <= vs_in;
-  hs_delay(0) <= hs_in;
-  de_delay(0) <= de_in;
+     -- first value of array is current input
+     vs_delay(1) <= vs_in;
+     hs_delay(1) <= hs_in;
+     de_delay(1) <= de_in;
 
-  for i in 0 to (delay-2) loop
-    vs_delay(i+1) <= vs_delay(i);
-    hs_delay(i+1) <= hs_delay(i);
-    de_delay(i+1) <= de_delay(i);
-  end loop;
+    -- delay according to generic
+    for i in 2 to delay loop
+      vs_delay(i) <= vs_delay(i-1);
+      hs_delay(i) <= hs_delay(i-1);
+      de_delay(i) <= de_delay(i-1);
+    end loop;
 
-end process;
+  end process;
 
-vs_out <= vs_delay(delay-1);
-hs_out <= hs_delay(delay-1);
-de_out <= de_delay(delay-1);
-  
+  -- last value of array is output
+  vs_out <= vs_delay(delay);
+  hs_out <= hs_delay(delay);
+  de_out <= de_delay(delay);
+
 end behave;
-
